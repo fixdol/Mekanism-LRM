bc69bb1b2180c3aa768f1adbeca3e3de2c39ad91
FixedDolphin927 <fixdol1127@gmail.com>
2024-11-23 11:52:13 +0900

    Fixed Creative Tab

--- src/main/java/com/fxd927/mlrm/common/registries/MLRMCreativeTab.java
+++ src/main/java/com/fxd927/mlrm/common/registries/MLRMCreativeTab.java
@@ -10,7 +10,7 @@ import mekanism.common.registries.MekanismCreativeTabs;
 public class MLRMCreativeTab {
     public static final CreativeTabDeferredRegister CREATIVE_TABS = new CreativeTabDeferredRegister(MekanismLiquidRadioactiveMaterial.MODID);
 
-    public static final CreativeTabRegistryObject MEKANISM_SCIENCE = CREATIVE_TABS.registerMain(MLRMLang.MLRM, MekanismBlocks.RADIOACTIVE_WASTE_BARREL, builder ->
+    public static final CreativeTabRegistryObject MLRM = CREATIVE_TABS.registerMain(MLRMLang.MLRM, MekanismBlocks.RADIOACTIVE_WASTE_BARREL, builder ->
             builder.withSearchBar()
                     .withTabsBefore(MekanismCreativeTabs.MEKANISM.key())
                     .displayItems((displayParameters, output) -> {
